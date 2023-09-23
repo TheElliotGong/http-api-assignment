40fb8dd651ad1c52c58f230a5f67856d96152269
thetyper101 <ewcgong@hotmail.com>
2023-09-23 12:10:03 -0400

    Added comments

--- src/responses.js
+++ src/responses.js
@@ -5,12 +5,22 @@ const respond = (request, response, status, content, type) => {
   response.write(content);
   response.end();
 };
-
+/**
+ * This function handles server requests that don't use query parameters.
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} type = the format that the returned data follows.
+ * @param {*} message = the message that is going to be returned data includes.
+ * @param {*} status = the HTTP status of the response.
+ * @returns 
+ */
 const nonQueryResponse = (request, response, type, message, status) => {
+  //Check if data needs to be formatted in xml
   if (type === 'text/xml') 
   {
     let xmlMessage = '<response>';
     xmlMessage += `<message>${message.message}</message>`;
+    //Include message id if possible
     if(message.id)
     {
       xmlMessage += `<id>${message.id}</id>`;
@@ -18,18 +28,34 @@ const nonQueryResponse = (request, response, type, message, status) => {
     xmlMessage += '</response>';
     return respond(request, response, status, xmlMessage, type);
   } 
+  //Return data in json format by default
   return respond(request, response, status, JSON.stringify(message), "application/json");
 }
-
+/**
+ * This function handles server requests that use query parameters.
+ * @param {*} request 
+ * @param {*} response 
+ * @param {*} type = the format that the returned data follows.
+ * @param {*} validMessage = the message to be returned when a query parameter is included.
+ * @param {*} invalidMessage = the message to be returned when a query parameter is missing.
+ * @param {*} params = the list of parameters in a url
+ * @param {*} attribute = the specific url parameter/attribute to check for
+ * @param {*} paramValue = the value the url parameter/attribute should be checked again.
+ * @param {*} statuses = the list of statuses depending on the presence of query parameters
+ * in the url.
+ * @returns 
+ */
 const queryResponse = (request, response, type, validMessage, invalidMessage, 
   params, attribute, paramValue, statuses) => {
   let status = statuses.validStatus;
   let message = validMessage;
+  //Check if query parameter is present.
   if(!params[attribute] || params[attribute] !== paramValue)
   {
     message = invalidMessage;
     status = statuses.invalidStatus;
   }
+  //Check if data needs to be formatted in xml
   if (type === 'text/xml') 
   {
     let xmlMessage = '<response>';
@@ -38,7 +64,7 @@ const queryResponse = (request, response, type, validMessage, invalidMessage,
 
     return respond(request, response, status, xmlMessage, type);
   } 
-  //Default output
+  //Return data in json format by default.
   return respond(request, response, status, JSON.stringify(message),
    "application/json");
 };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
+//Include other scripts and modules
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const responseHandler = require('./responses.js');
-
+//Fields for responses that don't use query parameters.
 const successMessage = {message: 'This is a successful response.'};
 const notFoundMessage = {message: 'The page you were looking for was not found.',
 id: 'notFound'};
@@ -14,24 +15,23 @@ id: 'internalError'};
 const notImplementedMessage = {message: `A get request for this page has not been implemented yet. 
 Check again later for updated content.`,
 id: 'notImplemented'};
-
+//Fields for requests that use query parameters.
 const badRequestValidMessage = {message: "This request has the required parameters."};
 const badRequestInvalidMessage = {message:"Missing valid query param set to true.",
 id: 'badRequest'};
 const badRequestStatuses = {validStatus: 200, invalidStatus: 400};
-
 const unauthorizedValidMessage = {message: "You have successfully viewed the content."};
 const unauthorizedInvalidMessage = {message: "Missed loggedIn query parameter set to yes.",
 id: 'unauthorized'};
 const unauthorizedStatuses = {validStatus: 200, invalidStatus: 401};
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-
+//This function handles requests to the server from both the url and the html page.
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
   const params = query.parse(parsedUrl.query);
   const acceptedType = request.headers.accept;
-  console.log(acceptedType);
+  //Handle various types of requests 
   switch (parsedUrl.pathname) {
     case '/':
       htmlHandler.getIndex(request, response);
