7e6323ab315e633427fd8b66134ecf4665fcce04
thetyper101 <ewcgong@hotmail.com>
2023-09-23 12:51:46 -0400

    Fixed all errors and readded console logs

--- src/responses.js
+++ src/responses.js
@@ -3,7 +3,7 @@ Author: Elliot Gong
 Purpose: Handle server responses that will return data in either xml or json
 Date: 9/23/2023
 */
-//Helper method to return stuff to the client.
+// Helper method to return stuff to the client.
 const respond = (request, response, status, content, type) => {
   response.writeHead(status, { 'Content-Type': type });
   response.write(content);
@@ -11,34 +11,34 @@ const respond = (request, response, status, content, type) => {
 };
 /**
  * This function handles server requests that don't use query parameters.
- * @param {*} request 
- * @param {*} response 
+ * @param {*} request
+ * @param {*} response
  * @param {*} type = the format that the returned data follows.
  * @param {*} message = the message that is going to be returned data includes.
  * @param {*} status = the HTTP status of the response.
- * @returns 
+ * @returns
  */
 const nonQueryResponse = (request, response, type, message, status) => {
-  //Check if data needs to be formatted in xml
-  if (type === 'text/xml') 
-  {
+  // Check if data needs to be formatted in xml
+  if (type === 'text/xml') {
     let xmlMessage = '<response>';
     xmlMessage += `<message>${message.message}</message>`;
-    //Include message id if possible
-    if(message.id)
-    {
+    // Include message id if possible
+    if (message.id) {
       xmlMessage += `<id>${message.id}</id>`;
     }
     xmlMessage += '</response>';
+    console.log(xmlMessage);
     return respond(request, response, status, xmlMessage, type);
-  } 
-  //Return data in json format by default
-  return respond(request, response, status, JSON.stringify(message), "application/json");
-}
+  }
+  // Return data in json format by default
+  console.log(message);
+  return respond(request, response, status, JSON.stringify(message), 'application/json');
+};
 /**
  * This function handles server requests that use query parameters.
- * @param {*} request 
- * @param {*} response 
+ * @param {*} request
+ * @param {*} response
  * @param {*} type = the format that the returned data follows.
  * @param {*} validMessage = the message to be returned when a query parameter is included.
  * @param {*} invalidMessage = the message to be returned when a query parameter is missing.
@@ -47,32 +47,46 @@ const nonQueryResponse = (request, response, type, message, status) => {
  * @param {*} paramValue = the value the url parameter/attribute should be checked again.
  * @param {*} statuses = the list of statuses depending on the presence of query parameters
  * in the url.
- * @returns 
+ * @returns
  */
-const queryResponse = (request, response, type, validMessage, invalidMessage, 
-  params, attribute, paramValue, statuses) => {
+const queryResponse = (
+  request,
+  response,
+  type,
+  validMessage,
+  invalidMessage,
+  params,
+  attribute,
+  paramValue,
+  statuses,
+) => {
   let status = statuses.validStatus;
   let message = validMessage;
-  //Check if query parameter is present.
-  if(!params[attribute] || params[attribute] !== paramValue)
-  {
+  // Check if query parameter is present.
+  if (!params[attribute] || params[attribute] !== paramValue) {
     message = invalidMessage;
     status = statuses.invalidStatus;
   }
-  //Check if data needs to be formatted in xml
-  if (type === 'text/xml') 
-  {
+  // Check if data needs to be formatted in xml
+  if (type === 'text/xml') {
     let xmlMessage = '<response>';
     xmlMessage += `<message>${message.message}</message>`;
     xmlMessage += `<id>${message.id}</id></response>`;
-
+    console.log(xmlMessage);
     return respond(request, response, status, xmlMessage, type);
-  } 
-  //Return data in json format by default.
-  return respond(request, response, status, JSON.stringify(message),
-   "application/json");
+  }
+  // Return data in json format by default.
+  console.log(message);
+  return respond(
+    request,
+    response,
+    status,
+    JSON.stringify(message),
+    'application/json',
+  );
 };
-module.exports = { 
-  respond,  
+module.exports = {
+  respond,
   nonQueryResponse,
-  queryResponse};
+  queryResponse,
+};
--- src/server.js
+++ src/server.js
@@ -4,40 +4,52 @@ Purpose: Handle server actions for various requests
 Date: 9/23/2023
 */
 
-//Include other scripts and modules
+// Include other scripts and modules
 const http = require('http');
 const url = require('url');
 const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const responseHandler = require('./responses.js');
-//Fields for responses that don't use query parameters.
-const successMessage = {message: 'This is a successful response.'};
-const notFoundMessage = {message: 'The page you were looking for was not found.',
-id: 'notFound'};
-const forbiddenMessage = { message: 'You do not have access to this content.',
-id: 'forbidden'};
-const internalMessage = {message: 'Internal Server Error. Something went wrong.',
-id: 'internalError'};
-const notImplementedMessage = {message: `A get request for this page has not been implemented yet. 
-Check again later for updated content.`,
-id: 'notImplemented'};
-//Fields for requests that use query parameters.
-const badRequestValidMessage = {message: "This request has the required parameters."};
-const badRequestInvalidMessage = {message:"Missing valid query param set to true.",
-id: 'badRequest'};
-const badRequestStatuses = {validStatus: 200, invalidStatus: 400};
-const unauthorizedValidMessage = {message: "You have successfully viewed the content."};
-const unauthorizedInvalidMessage = {message: "Missed loggedIn query parameter set to yes.",
-id: 'unauthorized'};
-const unauthorizedStatuses = {validStatus: 200, invalidStatus: 401};
+// Fields for responses that don't use query parameters.
+const successMessage = { message: 'This is a successful response.' };
+const notFoundMessage = {
+  message: 'The page you were looking for was not found.',
+  id: 'notFound',
+};
+const forbiddenMessage = {
+  message: 'You do not have access to this content.',
+  id: 'forbidden',
+};
+const internalMessage = {
+  message: 'Internal Server Error. Something went wrong.',
+  id: 'internalError',
+};
+const notImplementedMessage = {
+  message: 'A get request for this page has not been implemented yet.'
++ ' Check again later for updated content.',
+  id: 'notImplemented',
+};
+// Fields for requests that use query parameters.
+const badRequestValidMessage = { message: 'This request has the required parameters.' };
+const badRequestInvalidMessage = {
+  message: 'Missing valid query param set to true.',
+  id: 'badRequest',
+};
+const badRequestStatuses = { validStatus: 200, invalidStatus: 400 };
+const unauthorizedValidMessage = { message: 'You have successfully viewed the content.' };
+const unauthorizedInvalidMessage = {
+  message: 'Missed loggedIn query parameter set to yes.',
+  id: 'unauthorized',
+};
+const unauthorizedStatuses = { validStatus: 200, invalidStatus: 401 };
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-//This function handles requests to the server from both the url and the html page.
+// This function handles requests to the server from both the url and the html page.
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
   const params = query.parse(parsedUrl.query);
   const acceptedType = request.headers.accept;
-  //Handle various types of requests 
+  // Handle various types of requests
   switch (parsedUrl.pathname) {
     case '/':
       htmlHandler.getIndex(request, response);
@@ -46,23 +58,59 @@ const onRequest = (request, response) => {
       htmlHandler.getCSS(request, response);
       break;
     case '/success':
-      responseHandler.nonQueryResponse(request, response, acceptedType, successMessage, 200)
+      responseHandler.nonQueryResponse(request, response, acceptedType, successMessage, 200);
       break;
     case '/badRequest':
-      responseHandler.queryResponse(request, response, acceptedType, badRequestValidMessage, 
-        badRequestInvalidMessage, params, 'valid', 'true', badRequestStatuses);
+      responseHandler.queryResponse(
+        request,
+        response,
+        acceptedType,
+        badRequestValidMessage,
+        badRequestInvalidMessage,
+        params,
+        'valid',
+        'true',
+        badRequestStatuses,
+      );
       break;
     case '/badRequest?valid=true':
-      responseHandler.queryResponse(request, response, acceptedType, badRequestValidMessage, 
-        badRequestInvalidMessage, params, 'valid', 'true', badRequestStatuses);
+      responseHandler.queryResponse(
+        request,
+        response,
+        acceptedType,
+        badRequestValidMessage,
+        badRequestInvalidMessage,
+        params,
+        'valid',
+        'true',
+        badRequestStatuses,
+      );
       break;
     case '/unauthorized':
-      responseHandler.queryResponse(request, response, acceptedType, unauthorizedValidMessage, 
-        unauthorizedInvalidMessage, params, 'loggedIn', 'yes', unauthorizedStatuses);
+      responseHandler.queryResponse(
+        request,
+        response,
+        acceptedType,
+        unauthorizedValidMessage,
+        unauthorizedInvalidMessage,
+        params,
+        'loggedIn',
+        'yes',
+        unauthorizedStatuses,
+      );
       break;
     case '/unauthorized?loggedIn=yes':
-      responseHandler.queryResponse(request, response, acceptedType, unauthorizedValidMessage, 
-        unauthorizedInvalidMessage, params, 'loggedIn', 'yes', unauthorizedStatuses);
+      responseHandler.queryResponse(
+        request,
+        response,
+        acceptedType,
+        unauthorizedValidMessage,
+        unauthorizedInvalidMessage,
+        params,
+        'loggedIn',
+        'yes',
+        unauthorizedStatuses,
+      );
       break;
     case '/notImplemented':
       responseHandler.nonQueryResponse(request, response, acceptedType, notImplementedMessage, 501);
@@ -74,7 +122,7 @@ const onRequest = (request, response) => {
       responseHandler.nonQueryResponse(request, response, acceptedType, notFoundMessage, 404);
       break;
     case '/internal':
-      responseHandler.nonQueryResponse(request, response, acceptedType, internalMessage, 500);;
+      responseHandler.nonQueryResponse(request, response, acceptedType, internalMessage, 500);
       break;
     default:
       responseHandler.nonQueryResponse(request, response, acceptedType, notFoundMessage, 404);
